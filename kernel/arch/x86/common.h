08ad57510833b8c52fdffa0e05a5dd8f8a8579fe
SpiderPig1297 <spiderpig1297@gmail.com>
2021-01-30 07:36:06 -0800

    Added arch-dependent typedefs

---
+++ kernel/arch/x86/common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// 32-bit x86 typedefs.
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
