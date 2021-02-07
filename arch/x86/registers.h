ffea6a805fb42759bb03203eeab6059e4f245c5b
SpiderPig1297 <spiderpig1297@gmail.com>
2021-02-07 05:56:34 -0800

    Added setters and getters for cr0, cr2, cr3

--- arch/x86/registers.h
+++ arch/x86/registers.h
@@ -8,3 +8,28 @@ typedef struct {
     uint32_t int_number, err_code;                      // Interrupt number and error code.
     uint32_t eip, cs, eflags, useresp, ss;              // Pushed automatically by the CPU.
 } registers_t;  
+
+#define _DEFINE_CRX_GETTER(n)                                   \
+    uint32_t inline get_cr##n() {                               \
+        uint32_t cr##n;                                         \
+        __asm__ __volatile__("mov %%cr##n, %0" : "=r" (cr##n)); \
+        return cr##n;                                           \
+    }                                                           \
+
+#define _DEFINE_CRX_SETTER(n)                                   \
+    void inline set_cr##n(uint32_t val)                         \
+    {                                                           \
+        __asm__ __volatile__("mov %0, %%cr##n" :: "r" (val));   \
+    }                                                           \
+
+// Define getter and setter for CR0.
+_DEFINE_CRX_GETTER(0);
+_DEFINE_CRX_GETTER(0);
+
+// Define getter and setter for CR2.
+_DEFINE_CRX_GETTER(2);
+_DEFINE_CRX_GETTER(2);
+
+// Define getter and setter for CR3.
+_DEFINE_CRX_GETTER(3);
+_DEFINE_CRX_SETTER(3);
